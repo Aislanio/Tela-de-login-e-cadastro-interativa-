728de44d3aaa0b2dbc856a5d9984d85fbe443e12
Aislanio <aislanioamorim@gmail.com>
2020-09-20 15:28:15 -0300

    first commit

---
+++ testesite/js/index.js
@@ -0,0 +1,15 @@
+
+
+var btnSignin = document.querySelector("#singin");
+var btnSignup = document.querySelector("#singup");
+
+var body = document.querySelector("body");
+
+
+btnSignin.addEventListener("click", function () {
+   body.className = "sign-in-js"; 
+});
+
+btnSignup.addEventListener("click", function () {
+    body.className = "sign-up-js";
+})
